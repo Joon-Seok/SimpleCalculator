06057f9802ae71ba542d977189d7d058d77f96cc
SoheeKim <gsw6565@gmail.com>
2017-04-05 00:44:26 +0900

    fix bugs

--- calc.c
+++ calc.c
@@ -5,28 +5,30 @@ int main(){
 	FILE *fp = NULL;
 	int operand1, operand2;
 	char operator = ' ';
-	int result, line = 0;
+	int line = 0;
+	double result =0;
 
 	fp = fopen("read.txt","r");
 	if(fp!=NULL){
 		fscanf(fp, "%d", &line);
 	
-		for(int i=0; i<line; i++) {
+		for(int i=1; i<line; i++) {
 			fscanf(fp, "%d %c %d",&operand1, &operator, &operand2);
 			switch(operator) {
 				case '+':
-				result = add(operand1, operator);
+				result = add(operand1, operand2);
 				break;
 				case '-':
-				result = minus(operand1, operator);
+				result = minus(operand1, operand2);
 				break;
 				case '*':
-				result = mul(operand1, operator);
+				result = mul(operand1, operand2);
+				break;
 				case '/':
-				result = div(operand1, operator);
+				result = div(operand1, operand2);
 				break;
 			}		
-			printf("%d %c %d = %d\n",
+			printf("%d %c %d = %f\n",
 				 operand1, operator, operand2, result);
 		}
 	}
